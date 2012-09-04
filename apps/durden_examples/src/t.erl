172edc4abaf64107c1132680cc9d9d6710d876d3
Roman Gafiyatullin <r.gafiyatullin@me.com>
2012-09-04 20:03:10 +0300

    Fix GH#1

--- apps/durden_examples/src/t.erl
+++ apps/durden_examples/src/t.erl
@@ -2,14 +2,13 @@
 -compile(export_all).
 -include_lib("xmerl/include/xmerl.hrl").
 
-test() ->
-	durden_wsdl:module_wsdl("http://localhost:8080/test/service.asmx", soap_test_service).
-
 start() ->
 	Dispatch = [
 		%% {Host, list({Path, Handler, Opts})}
-		% {'_', [{[<<"test">>, <<"service.asmx">>, '...'], soap_test_service, [] }]},
-		{'_', [{[<<"pb">>, <<"v0.asmx">>, '...'], phone_book_soap, [] }]}
+		{'_', [
+			{[<<"test">>, <<"service.asmx">>, '...'], soap_test_service, [ "http://localhost:8080/test/service.asmx" ] },
+			{[<<"pb">>, <<"v0.asmx">>, '...'], phone_book_soap, [ "http://localhost:8080/pb/v0.asmx" ] }
+		]}
 	],
 	cowboy:start_listener(my_http_listener, 100,
 		cowboy_tcp_transport, [{port, 8080}],
